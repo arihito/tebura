a06353f4a7181fd53eb0644e87759cfc9be95a33
ArihitoMatsuda <arihito.m@gmail.com>
2019-09-09 22:08:01 +0900

    fadein on scroll

--- html/js/index.js
+++ html/js/index.js
@@ -48,4 +48,34 @@ $(function(){
     })
   })
 
+  // ---------------------------------------------------------------
+  // Fadein slide into Scroll
+  // ---------------------------------------------------------------
+  let slideConts = document.querySelectorAll('.slide')
+  console.log(slideConts)
+  let slideContsRect = []
+  let slideContsTop = []
+  let windowY = window.pageYOffset
+  let windowH = window.innerHeight
+  let remainder = 50
+  for (let i = 0; i < slideConts.length; i++) {
+    slideContsRect.push(slideConts[i].getBoundingClientRect())
+  }
+  for (let i = 0; i < slideContsRect.length; i++) {
+    slideContsTop.push(slideContsRect[i].top + windowY)
+  }
+  window.addEventListener('resize', function () {
+    windowH = window.innerHeight
+  });
+  window.addEventListener('scroll', function () {
+    windowY = window.pageYOffset
+    for (let i = 0; i < slideConts.length; i++) {
+      if(windowY > slideContsTop[i] - windowH + remainder) {
+        slideConts[i].classList.add('show')
+      } else {
+        slideConts[i].classList.remove('show')
+      }
+    }
+  })
+
 })
